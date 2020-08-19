0d890190996687108064cd50caf7997049e95f73
aineroonan <roonanaine@gmail.com>
2020-08-19 16:44:45 -0700

    exercise 10

---
+++ ex-10.R
@@ -0,0 +1,22 @@
+# Aine Roonan
+# August 19
+# Homework exercises day 10 geometries
+
+library(USAboundaries)
+library(tidyverse)
+library(sf)
+
+USAboundaries::us_states()
+conus = USAboundaries::us_states() %>%
+  filter(!state_name %in% c("Puerto Rico", "Alaska", "Hawaii"))
+
+states <- st_cast(states, 'MULTILINESTRING') %>% select(geometry)
+
+plot(states$geometry, col = 'lavender')
+states
+
+
+outline <- st_union(states$geometry) %>% st_cast('MULTILINESTRING')
+
+plot(outline, col = 'turquoise')
+outline
